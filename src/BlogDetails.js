78424f8272023ccd26f8d54be8ea3fa18b5b1314
Harshit Duggal <harshitduggal101@gmail.com>
2021-06-29 02:06:44 +0530

    router done

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -2,12 +2,12 @@ import { useParams } from "react-router-dom";
 
 const BlogDetails = () => {
     const { id } = useParams();
-
+    
     return ( 
-        <div className = 'blog-details'>
-            <h2>Blog Details - {id}</h2>
+       <div className = 'blog-details'>
+            {<h2>Blog Details - {id}</h2>}
         </div>
     );
 }
- 
+
 export default BlogDetails;
--- src/BlogList.js
+++ src/BlogList.js
@@ -2,15 +2,16 @@ import { Link } from "react-router-dom";
 const BlogList = ({blogs,title}) => {
     // const blogs= props.blogs;
     // const title= props.title;
-    return ( 
+    return (
         <div className='Blog-List'>
             <h2>{ title }</h2>
             {blogs.map((blog) => (
             <div className='Blog-Preview' key={blog.id}>
                 <Link to= { `/blogs/${blog.id}`} >
                     <h2>{blog.title}</h2>
+                    </Link>
                     <p>Written by {blog.author}</p>
-                </Link>
+                
             </div>
             ))}
         </div>    
